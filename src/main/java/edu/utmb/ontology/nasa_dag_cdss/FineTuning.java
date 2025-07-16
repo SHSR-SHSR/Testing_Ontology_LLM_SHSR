7bb1750cd0e0f67f9115e73ff62c662915fda8c9
ProfTuan <muhammad.f.amith@uth.tmc.edu>
2025-07-16 09:14:48 -0500

    bakcup

---
+++ src/main/java/edu/utmb/ontology/nasa_dag_cdss/FineTuning.java
@@ -0,0 +1,28 @@
+package edu.utmb.ontology.nasa_dag_cdss;
+
+import edu.utmb.ontology.hootation.core.Hootation;
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ * Hello world!
+ */
+public class FineTuning {
+    
+    private File ontology_file;
+    
+    public FineTuning(File ontology){
+        ontology_file = ontology;
+    }
+    
+    public ArrayList<String> convertAxiomsToNaturalLanguage(){
+        Hootation hootation = new Hootation();
+        ArrayList<String> natural_language_statements = hootation.get_naturalLangaugeStatements(ontology_file);
+        
+        return natural_language_statements;
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
